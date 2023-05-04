ce07c1779b6448e0945f49ca6ec9e199a68272a8
IvoGeorgiev <163478@vuzf.bg>
2023-05-04 12:47:57 +0300

    Initial

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+// const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+// const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+
+function HomePage() {
+  return <div>Helloooo</div>;
+}
+
+export default HomePage;
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDrCpo5n_QXdDnTXe6dpNBOW5Y0EPGHERw",
+  authDomain: "mama-bear-f2653.firebaseapp.com",
+  projectId: "mama-bear-f2653",
+  storageBucket: "mama-bear-f2653.appspot.com",
+  messagingSenderId: "684837688363",
+  appId: "1:684837688363:web:1b445bdb7e06bfbeb0c66e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
